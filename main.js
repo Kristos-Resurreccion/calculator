394cbbdcfffcbe909dd2994701548d44165ecf47
Kristos-Resurreccion <101231670+Kristos-Resurreccion@users.noreply.github.com>
2024-07-08 21:53:00 +1000

    Add funtion to update display with keypad click

--- main.js
+++ main.js
@@ -1,30 +1,45 @@
 const keypad = document.querySelector('.keypad');
 const operatorButtons = document.querySelector('.operators');
+const display = document.querySelector('.display');
 console.log(keypad);
 
 const operatorIcons = ['+', '-', 'x', '/', '='];
 function operators(operatorIcons) {
     for (const icon of operatorIcons) {
         const button = document.createElement('button');
-        button.classList.add(`operator-${icon}`);
+        button.classList.add(`operator`);
         button.textContent = icon;
-
+        button.addEventListener('click', e => {
+            display.textContent += ` ${e.target.textContent} `;
+        })
         operatorButtons.append(button);
     }
 }
 
 operators(operatorIcons);
 
-
 function numbers() {
     for (i = 9; i >= 0; i--) {
         const button = document.createElement('button');
-        button.classList.add(`button-${i}`);
+        button.classList.add(`button`);
         button.textContent = `${i}`;
+        button.addEventListener('click', e => {
+            display.textContent += e.target.textContent;
+        })
+
+
+
 
         keypad.append(button);
         console.log(i);
     }
+
+
 }
 
 numbers();
+
+
+
+
+
