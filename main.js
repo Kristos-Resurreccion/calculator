4254bac50038c9587d7ccd28f0b656f64da1ef06
Kristos-Resurreccion <101231670+Kristos-Resurreccion@users.noreply.github.com>
2024-07-19 18:13:15 +1000

    Recreate Calculator functions

--- main.js
+++ main.js
@@ -3,52 +3,54 @@ const operatorButtons = document.querySelector('.operators');
 const display = document.querySelector('.display');
 const clear = document.querySelector('.clear');
 
-let numberA;
+let numA;
 let operator;
-let numberB;
-let result;
+let numB;
 
-function addNumber(num) {
-    if (numberA == null) {
-        numberA = num;
-    } else {
-        numberA += num;
-    }
+function add(a, b) {
+    numb = a + b;
+    return Math.round((numb + Number.EPSILON) * 100) / 100;
+}
 
-    display.textContent += num;
-};
+function sub(a, b) {
+    numb = b - a;
+    return Math.round((numb + Number.EPSILON) * 100) / 100;
+}
 
-function operate(operation) {
-    if (numberB == null) {
-        numberB = numberA;
-        numberA = null;
-        operator = operation;
-        display.textContent = numberA;
-        display.textContent += `${operation} `;
-    } else {
-        numberA = parseInt(numberA);
-        numberB = parseInt(numberB);
-        console.log(operation);
-        console.log('a', numberA);
-        console.log('b', numberB);
-        switch (operator) {
-            case '+':
-                result = numberA + numberB;
-                break;
-            case '-':
-                result = numberB - numberA;
-                break;
-            case 'x':
-                result = numberA * numberB;
-                break;
-            case '/':
-                result = numberB / numberA;
-                break;
-        }
-        display.textContent = result;
-        console.log(result);
-        numberA = result;
-        numberB = null;
+function div(a, b) {
+    numb = b / a;
+    return Math.round((numb + Number.EPSILON) * 100) / 100;;
+}
+
+function mult(a, b) {
+    numb = b * a;
+    return Math.round((numb + Number.EPSILON) * 100) / 100;
+}
+
+function operate(a, b, oper) {
+    numA = parseInt(a);
+    numB = parseInt(b);
+    switch (oper) {
+        case '+':
+            numA = add(numA, numB);
+            numB = null;
+            display.textContent = numA;
+            break;
+        case '-':
+            numA = sub(numA, numB);
+            numB = null;
+            display.textContent = numA;
+            break;
+        case '/':
+            numA = div(numA, numB);
+            numB = null;
+            display.textContent = numA;
+            break;
+        case 'x':
+            numA = mult(numA, numB);
+            numB = null;
+            display.textContent = numA;
+            break;
     }
 }
 
@@ -59,8 +61,23 @@ function operators(operatorIcons) {
         button.classList.add(`operator`);
         button.textContent = icon;
         button.addEventListener('click', e => {
-            display.textContent += ` ${e.target.textContent} `;
-            operate(e.target.textContent);
+            if (numA != null) {
+
+                if (e.target.textContent == '=') {
+                    if (numB != null && numA != null && operator != null) {
+                        operate(numA, numB, operator);
+                    }
+                }
+                else if (numB == null) {
+                    numB = numA;
+                    numA = null;
+                    operator = e.target.textContent;
+                    display.textContent = ` ${e.target.textContent}`;
+                }
+
+
+            }
+
         })
         operatorButtons.append(button);
     }
@@ -74,7 +91,12 @@ function numbers() {
         button.classList.add(`button`);
         button.textContent = `${i}`;
         button.addEventListener('click', e => {
-            addNumber(e.target.textContent);
+            if (numA == null) {
+                numA = e.target.textContent;
+            } else {
+                numA += e.target.textContent;
+            }
+            display.textContent = numA;
         })
 
         keypad.append(button);
