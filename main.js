1d8c5c02f3d15a9070b03e1ccc60918c623b5542
Kristos-Resurreccion <101231670+Kristos-Resurreccion@users.noreply.github.com>
2024-07-19 17:07:07 +1000

    Add more functionality as well as a clear button

--- main.js
+++ main.js
@@ -1,7 +1,56 @@
 const keypad = document.querySelector('.keypad');
 const operatorButtons = document.querySelector('.operators');
 const display = document.querySelector('.display');
-console.log(keypad);
+const clear = document.querySelector('.clear');
+
+let numberA;
+let operator;
+let numberB;
+let result;
+
+function addNumber(num) {
+    if (numberA == null) {
+        numberA = num;
+    } else {
+        numberA += num;
+    }
+
+    display.textContent += num;
+};
+
+function operate(operation) {
+    if (numberB == null) {
+        numberB = numberA;
+        numberA = null;
+        operator = operation;
+        display.textContent = numberA;
+        display.textContent += `${operation} `;
+    } else {
+        numberA = parseInt(numberA);
+        numberB = parseInt(numberB);
+        console.log(operation);
+        console.log('a', numberA);
+        console.log('b', numberB);
+        switch (operator) {
+            case '+':
+                result = numberA + numberB;
+                break;
+            case '-':
+                result = numberB - numberA;
+                break;
+            case 'x':
+                result = numberA * numberB;
+                break;
+            case '/':
+                result = numberB / numberA;
+                break;
+        }
+        display.textContent = result;
+        console.log(result);
+        numberA = result;
+        numberB = null;
+    }
+}
 
 const operatorIcons = ['+', '-', 'x', '/', '='];
 function operators(operatorIcons) {
@@ -11,6 +60,7 @@ function operators(operatorIcons) {
         button.textContent = icon;
         button.addEventListener('click', e => {
             display.textContent += ` ${e.target.textContent} `;
+            operate(e.target.textContent);
         })
         operatorButtons.append(button);
     }
@@ -24,20 +74,32 @@ function numbers() {
         button.classList.add(`button`);
         button.textContent = `${i}`;
         button.addEventListener('click', e => {
-            display.textContent += e.target.textContent;
+            addNumber(e.target.textContent);
         })
 
-
-
-
         keypad.append(button);
-        console.log(i);
     }
 
 
 }
 
 numbers();
+
+function clearCalc() {
+    const button = document.createElement('button');
+    button.classList.add(`button`);
+    button.textContent = 'Clear';
+    button.addEventListener('click', e => {
+        numberA = null;
+        numberB = null;
+        result = null;
+        operator = null;
+        display.textContent = '';
+    })
+    clear.appendChild(button);
+}
+
+clearCalc();
 
 
 
